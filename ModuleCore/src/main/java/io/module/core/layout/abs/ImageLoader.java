afe329d4671fd6bca7b5cc2b11d1691bbfae1a6e
zt515 <kiva515@foxmail.com>
2017-10-27 00:14:46 +0800

    Core: Refactor project structure

--- ModuleCore/src/main/java/io/module/core/layout/abs/ImageLoader.java
+++
@@ -1,9 +0,0 @@
-package io.module.core.layout.abs;
-
-import android.widget.ImageView;
-
-public interface ImageLoader {
-    void loadImage(ImageView view, String url);
-
-    void loadRoundedImage(ImageView view, String url, int radius);
-}
